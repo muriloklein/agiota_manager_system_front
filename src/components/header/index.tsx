ef3049aa61be0e5691b2027596eb7edaf206b551
Murilo <eu.murilok@gmail.com>
2024-11-04 18:16:33 -0300

    Adicionado Componente de header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,35 @@
+import { useRouter } from "next/router";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
+import styles from "./styles.module.css";
+
+function Header() {
+  const router = useRouter();
+
+  return (
+    <Tabs
+      id="main-tabs"
+      activeKey={router.pathname}
+      onSelect={(selectedKey) => {
+        if (selectedKey) {
+          router.push(selectedKey);
+        }
+      }}
+      className={`${styles.headerContainer} mb-3`}
+      justify
+    >
+      <Tab
+        eventKey="/"
+        title={<span className={styles.tabLink}>Clientes</span>}
+        tabClassName={router.pathname === "/" ? styles.tabActive : ""}
+      />
+      <Tab
+        eventKey="/image"
+        title={<span className={styles.tabLink}>Imagem</span>}
+        tabClassName={router.pathname === "/image" ? styles.tabActive : ""}
+      />
+    </Tabs>
+  );
+}
+
+export default Header;
