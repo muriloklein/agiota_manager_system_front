6cf807601f165275dcb28339b3992a88436607ed
Murilo <eu.murilok@gmail.com>
2024-11-06 22:00:43 -0300

    Adicionado função de registrar usuário

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -9,6 +9,7 @@ import styles from "../styles/login.module.css";
 const Login = function () {
   const router = useRouter();
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [isRegister, setIsRegister] = useState(false);
 
   useEffect(() => {
     if (sessionStorage.getItem("ams-token")) {
@@ -26,25 +27,48 @@ const Login = function () {
     const params = { name, pin };
 
     const response = await authService.login(params);
-    const status = response?.status;
 
-    if (status === 200) {
-      router.push("/home");
+    const status = response?.status;
+    console.log(status);
+
+    if (status == 200) {
+      sessionStorage.setItem("ams-token", response.data.token);
+      router.push("/home").then(() => window.location.reload());
     } else {
       setErrorMessage("Usuário ou senha incorretos. Tente novamente.");
+    }
+  };
+
+  const handleRegister = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setErrorMessage(null);
+
+    const formData = new FormData(event.currentTarget);
+    const name = formData.get("usuario")!.toString();
+    const pin = formData.get("password")!.toString();
+    const params = { name, pin };
+
+    const response = await authService.register(params);
+    const status = response?.status;
+
+    if (status === 201) {
+      alert("Usuário registrado com sucesso!");
+      setIsRegister(false);
+    } else {
+      setErrorMessage("Erro ao registrar o usuário. Tente novamente.");
     }
   };
 
   return (
     <>
       <Head>
-        <title>AMS - Login</title>
+        <title>AMS - {isRegister ? "Registro" : "Login"}</title>
         <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
       </Head>
       <main className={styles.loginContainer}>
         <Container>
           <section className={styles.formes}>
-            <Form onSubmit={handleLogin}>
+            <Form onSubmit={isRegister ? handleRegister : handleLogin}>
               <p className={styles.title}>
                 <strong>
                   Bem-vindo(a) ao <br /> Agiota Manager System!
@@ -72,9 +96,18 @@ const Login = function () {
                 />
               </FormGroup>
               <Button className={styles.submitButton} type="submit" outline>
-                ENTRAR
+                {isRegister ? "REGISTRAR" : "ENTRAR"}
               </Button>
             </Form>
+            <Button
+              color="link"
+              className={styles.toggleButton}
+              onClick={() => setIsRegister(!isRegister)}
+            >
+              {isRegister
+                ? "Já tem uma conta? Faça login"
+                : "Não tem uma conta? Registre-se"}
+            </Button>
           </section>
         </Container>
       </main>
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,13 +1,32 @@
-// authService.ts
 import axios from "axios";
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3002";
 
 export const authService = {
   login: async (params: { name: string; pin: string }) => {
     try {
-      const response = await axios.get("/api/login", { params });
+      const response = await axios.post(`${API_URL}/login`, params);
       return response;
     } catch (error) {
-      return error.response;
+      if (axios.isAxiosError(error) && error.response) {
+        return error.response;
+      }
+    }
+  },
+
+  async register(params: { name: string; pin: string }) {
+    try {
+      const response = await axios.post(`${API_URL}/register`, params);
+      if (response.status === 201) {
+        return response;
+      }
+      console.log(response);
+    } catch (error: any) {
+      console.error(
+        "Erro ao registrar o usuário:",
+        error.response?.data || error.message
+      );
+      return null;
     }
   },
 };
